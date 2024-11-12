fabc31bb180cc6cfe20ddb1f2b95115e74dd7180
nawafmuflih <108142823+N4W4F@users.noreply.github.com>
2024-11-12 14:23:55 +0300

    Add files via upload

---
+++ src/main/java/com/example/exercise11introtospringboot/Exercise11IntroToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise11introtospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise11IntroToSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise11IntroToSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercise11introtospringboot/Exercise11IntroToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise11introtospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise11IntroToSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
